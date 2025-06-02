9fd5c7ff7b1977b8907a69f7b108815e90b0add7
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2025-06-02 14:46:42 -0300

    v0.003

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function acessibilidade() {};
+function acessibilidade() {
+    var opcoes = document.querySelector('.opcoes-acessibilidade');
+    opcoes.style.display = opcoes.style.display === 'block' ? 'none' : 'block';
+};
 
-function 
+function aumentarFonte() {
+    var body = document.querySelector('body');
+    var currentSize = window.getComputedStyle(body).fontSize;
+    var newSize = parseFloat(currentSize) * 1.2; // Aumenta o tamanho da fonte em 20%
+    body.style.fontSize = newSize + 'px';
+}
+
+function diminuirFonte() {
+    var body = document.querySelector('body');
+    var currentSize = window.getComputedStyle(body).fontSize;
+    var newSize = parseFloat(currentSize) * 0.8; // Diminui o tamanho da fonte em 20%
+    body.style.fontSize = newSize + 'px';
+}
